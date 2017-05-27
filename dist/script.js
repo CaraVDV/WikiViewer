87aea04cadcf3449594fda7a9bc45e2b13f1b2da
Cara V <caralvdv@gmail.com>
2017-05-26 18:52:41 -1000

    added icon to search bar transition

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,31 @@
 // script
 /***************
 dependencies: 
-
-
+Jquery
 ****************/
+$( document ).ready(function() {
+
+  $(".icon").on("click", function(){ iconToBar(); });
+  $("#x").on("click", function(){ barToIcon(); });
+
+	function iconToBar(){
+		console.log("icon clicked.");
+		$("#search-bar").removeClass("small");
+		$(".icon").addClass("hide");
+		setTimeout(function(){
+			$("#x").removeClass("hide");
+			$("#search-input").removeClass("hide");
+		}, 400);
+		
+	}
+
+	function barToIcon(){
+		console.log("x button clicked.");
+		$("#x").addClass("hide");
+		$("#search-input").addClass("hide");
+		$("#search-bar").addClass("small");
+		setTimeout(function(){
+			$(".icon").removeClass("hide");
+		}, 400);
+	}
+});
